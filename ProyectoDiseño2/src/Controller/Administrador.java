f41bf3462c0c83e235c1329c978d39d15ed566f2
Andres <Andres@Andy-PC.Home>
2016-06-11 15:01:25 -0600

    El controlador ya tiene las firmas de los metodos y la estructura según el diagrama,

---
+++ ProyectoDiseño2/src/Controller/Administrador.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Andres
+ */
+public class Administrador {
+    private double tipoDeCambio;
+    private Date fecha;
+    
+    public Administrador(){
+        
+    }
+    public Prestamo crearPrestamo(){
+        
+    }
+    public void escribirArchivo(){
+        
+    }
+    public void escribirPantalla(){}
+    
+    public void actualizarTipodDeCambio(){
+        
+    }
+    public void actualizarFecha(){
+        
+    }
+  
+}
